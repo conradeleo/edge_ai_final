c96c003c79ab622dcadcf161ca2c381e68c03822
fangfangirl <138897852+fangfangirl@users.noreply.github.com>
2025-06-03 00:19:04 +0800

    Update upload_model.py

--- upload_model.py
+++ upload_model.py
@@ -4,6 +4,6 @@ from huggingface_hub import HfApi
 api = HfApi()
 
 api.upload_folder(
-    repo_id=f"Fang77777/Llama-3.2-3B-Instruct-GPTQ-all-exllama", # 修改成自己的repo
-    folder_path="models/Llama-3.2-3B-Instruct-GPTQ-all", # 進行量化的模型
+    repo_id="your repo name",  # Change to your own repo
+    folder_path="models/Llama-3.2-3B-Instruct-Quan-2.8bits-1100-all",  # The quantized model folder to upload
 )
