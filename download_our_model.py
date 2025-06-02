3dd149a915c89eda68d032cc9abe818dedfae180
fangfangirl <138897852+fangfangirl@users.noreply.github.com>
2025-06-03 00:14:57 +0800

    Update download_our_model.py

--- download_our_model.py
+++ download_our_model.py
@@ -1,5 +1,6 @@
 from huggingface_hub import snapshot_download
 
+# === Download the our quantized model into a specified directory === #
 snapshot_download(
     repo_id="Fang77777/EAI_Final_model",
     local_dir="models/EAI_Final_model",
